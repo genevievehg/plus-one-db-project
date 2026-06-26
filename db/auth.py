b6c2eb025a1f0d846a1b3a83bbe3e388644fcc45
genevievehg <genevievegarwood@gmail.com>
2026-06-26 18:47:01 +0100

    feat: add get_current_user function

--- db/auth.py
+++ db/auth.py
@@ -1,6 +1,7 @@
 import bcrypt, os, jwt
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
 
 JWT_SECRET = os.getenv('JWT_SECRET')
 JWT_ALGORITHM = 'HS256'
@@ -17,3 +18,13 @@ def create_access_token(id:int) -> str:
     payload = {'sub': str(id), 'exp': expire}
     return jwt.encode(payload, JWT_SECRET, algorithm = JWT_ALGORITHM)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = 'api/auth/login')
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> int:
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms = [JWT_ALGORITHM])
+        return int(payload['sub'])
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code = 401, detail = 'Token has expired')
+    except jwt.InvaludeTokenError:
+        raise HTTPException(statud_code = 401, detail = 'Could not validate token')
