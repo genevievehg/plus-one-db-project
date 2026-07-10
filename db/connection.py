52a5769212c643e8df4358e51f045494a00e0b4c
genevievehg <genevievegarwood@gmail.com>
2026-07-10 16:16:55 +0100

    refactor: update connection config to run in  different enivironments

--- db/connection.py
+++ db/connection.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-database_name = os.environ["DATABASE_NAME"]
-
 def get_connection():
-    return psycopg2.connect(dbname=database_name)
+    return psycopg2.connect(dbname=os.getenv("PG_DATABASE"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+    port=os.getenv("PG_PORT"))
 
