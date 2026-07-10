d21fe0c055b67e913a0da827b280b92b56ac5c3a
genevievehg <genevievegarwood@gmail.com>
2026-07-10 19:50:36 +0100

    feat: add security group for database acces for ec2 and modify http ports

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -30,7 +30,28 @@ resource "aws_vpc_security_group_egress_rule" "allow_ssh_egress" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 8000
+  to_port           = 8000
   ip_protocol       = "tcp"
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds"
+  description = "Allow inbound PostgreSQL traffic from the EC2 security group"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_postgresql_ingress" {
+  security_group_id = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.allow_traffic.id
+
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_postgresql_egress" {
+  security_group_id = aws_security_group.rds.id
+
+  ip_protocol = "-1"
+  cidr_ipv4   = "0.0.0.0/0"
 }
