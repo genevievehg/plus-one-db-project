2e39e8dc009927dfa08925dbca7c50c932d8977c
genevievehg <genevievegarwood@gmail.com>
2026-07-12 23:45:27 +0100

    feat: update ec2 configuration

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,16 +14,22 @@ resource "aws_instance" "example" {
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_traffic.id]
   key_name               = "EC2_sprint"
-  user_data              = <<-EOF
-              #!/bin/bash
-              EOF
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  user_data              = templatefile("user_data.sh", {
+    db_endpoint = aws_db_instance.ncplusonedb.address
+    db_port     = aws_db_instance.ncplusonedb.port
+    db_name     = aws_db_instance.ncplusonedb.db_name
+    secret_name = "nc-plus-one-db"
+  })
+
+              
 
   user_data_replace_on_change = true
 
   tags = {
     Name = "nc-plus-one"
   }
-}
+  }
 
 output "public_ip" {
   value = aws_instance.example.public_ip
